d8ecbfdce2b0ae937b5491487a5541faab68da7f
mohamedhamedenho <mo7amed.7amedenho@gmail.com>
2024-03-21 17:15:07 +0200

    git push

---
+++ app/page2/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-xl px-4 font-bold text-center  text-white flex justify-center" style={{ textAlign: "center" , filter: "drop-shadow(0 0 0.75rem #ffffff)"}}>كل سنه ونتى طيبه بمناسبه عيد الأم
+        <br />
+        والله لو عملتى فيديوهات مع البت دى هنفخك انتى حره
+      </h1>
+    </div>
+  );
+};
+
+export default page;
